ba608ffe0f535db96aa9624ac8ed05b941945d9b
apilbasnet <apilbasnet10@gmail.com>
2024-01-10 13:27:15 +0545

    s

--- src/App.jsx
+++ src/App.jsx
@@ -172,7 +172,7 @@ function App() {
             className='flex-1 mr-1 pl-2' />
 
             <button onClick={handleAdd}  className='py-1 rounded-none'>
-              Add
+              Add data
             </button>
         </div>
         <ol className=''>
